736d52921adb559b98d9219552cd7ab671ce7f8b
kimsezin <48678553+kimsezin@users.noreply.github.com>
2021-07-23 14:50:14 +0900

    Using FVector,Deltatime for Actor move
    FVector, FMath Sin, Deltatime사용법 익히기

---
+++ Source/Class/FloatingActor.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FloatingActor.h"
+
+// Sets default values
+AFloatingActor::AFloatingActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	FloatingSpeed = FVector(1.0f, 1.0f, 1.0f);
+
+}
+
+// Called when the game starts or when spawned
+void AFloatingActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFloatingActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector NewLocation = GetActorLocation();
+	float DeltaX = (FMath::Sin(RunningTime.X + DeltaTime * FloatingSpeed.X) - FMath::Sin(RunningTime.X));
+	float DeltaY = (FMath::Sin(RunningTime.Y + DeltaTime * FloatingSpeed.Y) - FMath::Sin(RunningTime.Y));
+	float DeltaZ = (FMath::Sin(RunningTime.Z + DeltaTime * FloatingSpeed.Z) - FMath::Sin(RunningTime.Z));
+	NewLocation += FVector(DeltaX, DeltaY, DeltaZ) * 20.0f;
+	RunningTime += DeltaTime * FloatingSpeed;// 틱함수가 몇초마다 호출되는지에 대한 시간
+							 // 러닝타임에 실행되고난 뒤 얼마나 많은 시간이 흘렀는지에 대한 정보 들어감
+	SetActorLocation(NewLocation);
+	
+
+}
+
